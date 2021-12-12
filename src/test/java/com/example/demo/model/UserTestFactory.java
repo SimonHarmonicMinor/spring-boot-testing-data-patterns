b9839ef2008c780780e45045d8041ffb4f0fd98b
Semyon Kirekov <semyon@kirekov.com>
2021-12-12 17:54:34 +0300

    Add UserTestFactory

---
+++ src/test/java/com/example/demo/model/UserTestFactory.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+public class UserTestFactory {
+
+  public static User createUser(String login, String firstName, String lastName) {
+    final var user = new User();
+    user.setLogin(login);
+    user.setFirstName(firstName);
+    user.setLastName(lastName);
+    return user;
+  }
+}
